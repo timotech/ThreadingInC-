49b8ce8d071e9450584e53b8dae852af24ea3ad9
Timothy Ayodele <timotech@yahoo.com>
2022-06-13 10:04:54 +0100

    Add project files.

---
+++ ThreadingComplete/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingComplete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(WriteY);
+            t.Start();
+
+            for (int i = 0; i < 1000; i++) Console.Write("x");
+            Console.Read();
+        }
+
+        static void WriteY()
+        {
+            for (int i = 0; i < 1000; i++) Console.Write("y");
+        }
+    }
+}
